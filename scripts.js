4448d43ea679c28ec0348204338b34f0413cbb6b
rhit-nityareddy <buthalsr@rose-hulman.edu>
2025-09-26 12:24:16 -0400

    Added collapsible code, need to test

--- scripts.js
+++ scripts.js
@@ -1,1 +1,22 @@
+/**
+ * https://www.w3schools.com/howto/howto_js_collapsible.asp
+ */
+
+let coll = document.getElementsByClassName("collapsible");
+let i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("activeCollapsible");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+
+
 
