2a359ed01ecea0bba06bf28770760c2302e7151e
rhit-nityareddy <buthalsr@rose-hulman.edu>
2025-09-26 12:42:21 -0400

    Added more collapsibles

--- scripts.js
+++ scripts.js
@@ -8,7 +8,7 @@ let i;
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("activeCollapsible");
-    var content = this.nextElementSibling;
+    let content = this.nextElementSibling;
     if (content.style.display === "block") {
       content.style.display = "none";
     } else {
