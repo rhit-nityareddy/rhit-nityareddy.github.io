5d23a0179ba3d1ce56f4303175c79dc7c0132a5f
rhit-nityareddy <buthalsr@rose-hulman.edu>
2025-09-27 16:20:31 -0400

    fixed navbar

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
 /*
-* https://www.w3schools.com/jsref/dom_obj_pushbutton.asp
+* https://www.w3schools.com/jsref/prop_html_innerhtml.asp
 */
-let b = document.createElement("mainButton")
-b.addEventListener("click", () => {
-    
-})
+let nav = `<a href="index.html">Main</a>
+      <a href="resume.html">Resume</a>
+      <a href="portfolio.html">Portfolio</a>`;
+document.getElementById("nav").innerHTML = nav;
 
 
 /**
