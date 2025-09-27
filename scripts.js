28241246bba2382f8ab0acdf8d13a57df81274eb
rhit-nityareddy <buthalsr@rose-hulman.edu>
2025-09-27 16:59:02 -0400

    fixed error

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ window.addEventListener("load", function() {
   let nav = `<a href="index.html">Main</a>
       <a href="resume.html">Resume</a>
       <a href="portfolio.html">Portfolio</a>`;
-  document.getElementsByClassName[0]("nav").innerHTML = nav;
+  document.getElementsByClassName("nav")[0].innerHTML = nav;
 })
 
 
